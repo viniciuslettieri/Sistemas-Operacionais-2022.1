95c3187018a55d06890ce1f93e5eb2c37fa58e13
viniciuslettieri <viniciuslettieri@yahoo.com.br>
2022-07-04 21:46:18 -0300

    adicionando arquivo principal

---
+++ Trabalho 2/Source/Principal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* 
+
+* Definir uma estrutura TabelaPaginas
+    Cada entrada possui um marcador se está na memória
+
+* Definir uma estrutura Processo
+    Processo possui uma tabela de paginas
+
+*/
+
+
+
+int main() {
+
+    return 0;
+}
